5948d12c19e86da5b08195825422835d58714094
Peter MacKenzie <peter.mackenzie@gmail.com>
2016-04-14 02:36:38 +0100

    commit

--- statusapp/views.py
+++ statusapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from .models import Event
+from models import Event
 
 import datetime
 

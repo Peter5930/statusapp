a35a623921ea096367beff92200d5030329d5456
Peter MacKenzie <peter.mackenzie@gmail.com>
2016-04-14 01:44:44 +0100

    commit

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_extensions',
-    'django_comments',
+    #'django_comments',
     'disqus',
     'statusapp',
 )
